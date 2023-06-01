6d63d31b1bd5b16d4445c8b8b941bf5a8834c92d
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-06-01 14:23:40 +0100

    profile

--- pages/api/configProfiles/getConfigProfile.js
+++ pages/api/configProfiles/getConfigProfile.js
@@ -5,7 +5,7 @@ const xata = getXataClient()
 const handler = async (req, res) => {
   try {
     const id = req.query.id || req.body.id; // Check query parameter and request body
-    const data = await xata.db.ConfigProfiles.select(["*", "location.*", "left_neighbour.map_position_id","left_neighbour.ip", "right_neighbour.map_position_id","right_neighbour.ip" ]).filter({ id: id }).getMany(); //get only map_ids
+    const data = await xata.db.ConfigProfiles.select(["*"]).filter({ id: id }).getMany(); //get only map_ids
     res.json({ data });
   } catch (error) {
     res.status(500).json({ message: error.message, data: [] });
