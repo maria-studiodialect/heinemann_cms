2e3433de4bbd42df7ad87a662ddafbefb2ffeda6
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-06-27 09:56:52 +0100

    carousels

--- pages/api/products/updateProduct.js
+++ pages/api/products/updateProduct.js
@@ -3,13 +3,14 @@ import { getXataClient } from '../../../utils/xata'
 const xata = getXataClient()
 
 const handler = async (req, res) => {
-  // using update method to update records in database
+  // using update method to update records in the database
   const { id, ...data } = req.body
   try {
     await xata.db.Products.update(id, { ...data })
     res.json({ message: 'Success 😁' })
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    const errorMessage = 'Error updating the product.'
+    res.status(500).json({ message: errorMessage })
   }
 }
 
